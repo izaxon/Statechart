c3d911d1773f380bed246d1e23d905de60258b33
Johan Isaksson <johan@izaxon.com>
2024-12-17 23:42:19 +0100

    Simplify Transition class by removing unused properties and constructor logic

--- statechart-lib/src/state/transition.ts
+++ statechart-lib/src/state/transition.ts
@@ -1,21 +1,11 @@
+import { State } from "./state";
+
 // transition.ts
 
 export class Transition {
-  fromState: string;
-  toState: string;
-  event: string;
-
   constructor(
     public to: State | null = null,
     public condition: (() => boolean) | null = null,
     public action: (() => void) | null = null
-  ) {
-    this.fromState = fromState;
-    this.toState = toState;
-    this.event = event;
-  }
-
-  isTriggered(event: string): boolean {
-    return this.event === event;
-  }
+  ) {}
 }
