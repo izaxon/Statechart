19f2eeaab57d3c2c07b2fc29fd31e71437999b54
Johan Isaksson <johan@izaxon.com>
2024-12-17 23:39:12 +0100

    Refactor Transition class constructor to use public properties and improve readability

--- statechart-lib/src/state/state.ts
+++ statechart-lib/src/state/state.ts
@@ -1,19 +1,44 @@
+import { Transition } from "./transition";
+
 export class State {
-    name: string;
-    onEnter?: () => void;
+  readonly states: State[] = [];
+  readonly transitions: Transition[] = [];
+  current: State | null = null;
 
-    constructor(name: string, onEnter?: () => void) {
-        this.name = name;
-        this.onEnter = onEnter;
+  constructor(
+    public readonly name: string,
+    public entering: (() => void) | null = null,
+    public within: (() => void) | null = null,
+    public exiting: (() => void) | null = null,
+    transitions: Transition[] | null = null
+  ) {
+    if (transitions) {
+      this.transitions.push(...transitions);
+    }
+  }
+
+  protected run(): void {
+    if (this.within) {
+      this.within();
     }
 
-    enter() {
-        if (this.onEnter) {
-            this.onEnter();
+    if (this.current) {
+      for (const t of this.current.transitions) {
+        if (!t.condition || t.condition()) {
+          if (this.current && this.current.exiting) {
+            this.current.exiting();
+          }
+          this.current = t.to;
+          if (t.action) {
+            t.action();
+          }
+          if (this.current && this.current.entering) {
+            this.current.entering();
+          }
+          return;
         }
+      }
+      this.current.run();
     }
-
-    exit() {
-        // Logic for exiting the state can be added here
-    }
+  }
 }
--- statechart-lib/src/state/transition.ts
+++ statechart-lib/src/state/transition.ts
@@ -1,17 +1,21 @@
 // transition.ts
 
 export class Transition {
-    fromState: string;
-    toState: string;
-    event: string;
+  fromState: string;
+  toState: string;
+  event: string;
 
-    constructor(fromState: string, toState: string, event: string) {
-        this.fromState = fromState;
-        this.toState = toState;
-        this.event = event;
-    }
+  constructor(
+    public to: State | null = null,
+    public condition: (() => boolean) | null = null,
+    public action: (() => void) | null = null
+  ) {
+    this.fromState = fromState;
+    this.toState = toState;
+    this.event = event;
+  }
 
-    isTriggered(event: string): boolean {
-        return this.event === event;
-    }
+  isTriggered(event: string): boolean {
+    return this.event === event;
+  }
 }
