63575e26bda66f31164025bc17d137de07a26170
Johan Isaksson <johan@izaxon.com>
2024-12-18 09:54:51 +0100

    Remove unused StateConfig and TransitionConfig interfaces from index.ts

--- statechart-lib/src/types/index.ts
+++ statechart-lib/src/types/index.ts
@@ -1,19 +1,3 @@
 // src/types/index.ts
 
-export interface StateConfig {
-  name: string;
-  onEnter?: () => void;
-}
-
-export interface TransitionConfig {
-  fromState: string;
-  toState: string;
-  event: string;
-}
-
-// Rename the Transition type to TransitionType
-export type TransitionType = {
-  event: string;
-  from: string;
-  to: string;
-};
+// File can be empty if these were the only types defined
