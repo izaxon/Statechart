b1eaae171276a0a6e6a46c925830ba1326a20475
Johan Isaksson <johan@izaxon.com>
2024-12-21 09:49:41 +0100

    Bump version to 1.0.4 and enhance StateMachine to allow adding states dynamically

--- statechart-lib/src/machine.ts
+++ statechart-lib/src/machine.ts
@@ -3,15 +3,22 @@ import State from "./state";
 export default class StateMachine extends State {
   constructor(states: State[] = []) {
     super();
-    if (states.length === 0) {
-      throw new Error("Statechart must have at least one state.");
-    }
-
-    this.current = states[0];
     this.states.push(...states);
   }
 
+  public addState(state: State | State[]) {
+    this.states.push(...(Array.isArray(state) ? state : [state]));
+    return this;
+  }
+
   public update() {
+    if (!this.current) {
+      if (this.states.length === 0) {
+        throw new Error("Statechart must have at least one state.");
+      }
+      this.current = this.states[0];
+    }
+
     this.run();
   }
 }
